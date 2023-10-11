b0fdb463946cc380f1d9db5c0a9f1ae05bd0de78
Lagger <102471956+realLaggermaster@users.noreply.github.com>
2023-10-11 14:49:10 +0300

    Update and rename Uus projekt.js to Addition of number arrays.js

---
+++ Addition of number arrays.js
@@ -0,0 +1,25 @@
+NumberArray = [];
+
+numbers = prompt('Write here a first number:')
+if (numbers != '')
+NumberArray.push(numbers)
+
+numbers2 = prompt('Write here a second number:')
+if (numbers2 != '')
+NumberArray.push(numbers2)
+
+numbers3 = prompt('Write here a third number:')
+if (numbers3 != '')
+NumberArray.push(numbers3)
+
+console.log('You chose', NumberArray)
+
+calculated = Number(arvud) + Number(arvud2) + Number(arvud3)
+console.log('Results:', calculated)
+
+
+
+
+
+
+
