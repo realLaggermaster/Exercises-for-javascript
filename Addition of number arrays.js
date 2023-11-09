2ab285d7e1315d4ab4772f70e3ba8fef6c551496
Lagger <102471956+realLaggermaster@users.noreply.github.com>
2023-11-09 13:21:51 +0200

    Update Addition of number arrays.js

--- Addition of number arrays.js
+++ Addition of number arrays.js
@@ -1,3 +1,20 @@
+// New code
+let NumberArray = [];
+
+for (let i = 1; i < 4; i++) {
+    numbers = prompt('Write a number here');
+    if (numbers !== '') {
+        NumberArray.push(numbers)
+    }
+}
+
+console.log('You chose: '+ NumberArray);
+
+calculation = Number(NumberArray[0]) + Number(NumberArray[1]) + Number(NumberArray[2]);
+console.log('Result: '+ calculation);
+
+
+// Old code
 NumberArray = [];
 
 numbers = prompt('Write here a first number:')
